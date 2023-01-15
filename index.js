d963fbec67e9eae40ec7d5e0605236a6f9ada840
Joselito0015 <luis.blas@pucp.edu.pe>
2023-01-15 00:13:18 -0500

    Finished v1

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-console.log(process.env.OPENAI_API_KEY)
 
 // Crea una configuración con tu API Key de OpenAI
 const configuration = new Configuration({
@@ -18,26 +17,20 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
-
-// function generatePrompt() {
-//   return `Hablame como si fueras un consultor ambiental. Invitalos a obtener los servicios de la consultoría Carranza con el número +511-4601863`;
-// }
-
-
 // Define una ruta para manejar peticiones
-app.get('/completion', async (req, res) => {
+app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
-app.get('/ia', async (req, res) => {
-  const completion = await openai.createCompletion({
+app.post('/', async (req, res) => {
+
+  console.log("REQUEST!!!!!!!!")  
+  const completion = await  openai.createCompletion({
     max_tokens: 3800,
     model: "text-davinci-003",
     prompt: req.query.prompt,
-    temperature: 0.9,
-    // top_p: 1,
-    // stop: ["\n"]
+    temperature: 0.9
   });
 
   // Obtener la respuesta en formato de texto
@@ -52,7 +45,7 @@ app.get('/ia', async (req, res) => {
 
 
 // Inicia el servidor escuchando en un puerto específico
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(8282, () => {
+  console.log('Server started on port 8282');
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -2,29 +2,100 @@
 const industria= $('#InputIndustria')
 const problema= $('#InputProblema')
 const detalles= $('#InputDetalles')
+
+const Btn1= $('#Btn1')
+const Btn2= $('#Btn2')
 const BtnSubmit= $('#submit')
 const Last_modal= $('#Last_modal')
 
 
-console.log("gaaaaaaaas")
+const ContactarLast= $('#ContactarLast')
+const NewConsult= $('#NewConsult')
+
+
 // Eventos
-BtnSubmit.on("click",  async ( )=> {
+industria.on("input",()=>{
+  if (industria.val().trim() === ""){
+    Btn1.attr("disabled", true);
+  }
+  else{
+    Btn1.removeAttr("disabled")  
+  }
 
-  await fetch(`http://127.0.0.1:3000/ia?prompt=${generatePrompt()}`)
-  .then(response => response.text())
-  .then(data => {
-    // Aquí manejas la respuesta obtenida
-    console.log(data);
-    Last_modal.text(data)
+})
 
-  })
-  .catch(error => {
-    // Aquí manejas el error
-    console.error(error);
-  });
+problema.on("input",()=>{
+  if (problema.val().trim() === ""){
+    Btn2.attr("disabled", true);
+  }
+  else{
+    Btn2.removeAttr("disabled")  
+  }
 
-  
-  console.log("response222")
+})
+
+detalles.on("input",()=>{
+  if (detalles.val().trim() === ""){
+    BtnSubmit.attr("disabled", true);
+  }
+  else{
+    BtnSubmit.removeAttr("disabled")    
+
+  }
+
+})
+
+
+NewConsult.on("click", ()=>
+{
+  Last_modal.html(`<div id="spiner" class="spinner-border text-success" role="status"> <span class="visually-hidden">Loading...</span></div>`)
+  NewConsult.attr("disabled", true);
+  ContactarLast.attr("disabled", true);
+
+  const cursor= $('.typed-cursor')
+  cursor.remove()
+
+}
+)
+
+
+BtnSubmit.on("click",  async (e )=> {
+    await fetch(`${document.location.protocol}//${document.location.hostname}:8282/?prompt=${generatePrompt()}`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.text())
+    .then(data => {
+      // Aquí manejas la respuesta obtenida
+      console.log(data);
+      Last_modal.text("")
+
+      let options = {
+        strings: [data],
+        typeSpeed: 40,
+        onComplete: function() {
+          // Change button state here
+          NewConsult.removeAttr("disabled");
+          ContactarLast.removeAttr("disabled");
+          industria.val("")
+          problema.val("")
+          detalles.val("")
+
+          ContactarLast.addClass("btn-success")
+          ContactarLast.removeClass("btn-primary")
+
+      }
+      };
+      
+      let typed = new Typed('#Last_modal', options);
+
+      
+    })
+    .catch(error => {
+      // Aquí manejas el error
+      console.error(error);
+    });
+
 })
 
 
@@ -36,7 +107,7 @@ function generatePrompt() {
     - 3. Detalles Extra ${detalles.val()}
 
     Da la solución de forma objetiva, profesional y directa.
-    Primero da la solución de forma objetiva, al final invitalos a obtener nuestros servicios de la consultoría Carranza con el número +511-4601863.
+    Primero da la solución de forma objetiva, al final invitalos a obtener nuestros servicios de la consultoría Carranza con el número +51 994-615-074.
     Dame la resupuesta en menos de 1000 caracteres.`;
   }
   
